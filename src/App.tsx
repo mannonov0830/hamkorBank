d1edb7d4302c6e38a6aebc82e73209dc83c74203
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-06-11 21:55:57 +0500

    main

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 const App = () => {
   return (
-    <
+    <Navbar/>           
   )
 };
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,39 @@
+import logo from "./assets/logo.svg";
+const arr = [
+  "Kreditlar",
+  "Kartalar",
+  "Avtokreditlar",
+  "Ipoteka",
+  "Omonatlar",
+  "Valyutalar kursi",
+  "Yana",
+];
+const Navbar = () => {
+  return (
+    <nav className="navbar container">
+      <div className="logFlex">
+        <img src={logo} alt="" />
+        {arr.map((item, inedx) => (
+          <ul>
+            <li key={inedx}>{item}</li>
+          </ul>
+        ))}
+      </div>
+      <div className="btnFlex">
+        <div className="btn">
+          <div>
+            <h4>Sotib olish</h4>
+            <p>12 339</p>
+          </div>
+          <div>
+            <h4>Sotish</h4>
+            <p>12 356</p>
+          </div>
+        </div>
+        <button>Onlayn arizalar</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
