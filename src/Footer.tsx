c58ff80642cac79ab8090a37b09596e538ce3887
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-06-12 11:24:49 +0500

    dsadad

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,8 @@
-
 import logo from "./assets/mobile_app.svg";
 const Footer = () => {
   return (
     <footer className="bank-footer">
       <div className="footer-container">
-        
         <div className="footer-column contact-column">
           <div className="phone-group">
             <span className="phone-number">0 (800) 1 200 200, 1256</span>
@@ -18,36 +16,61 @@ const Footer = () => {
 
         <div className="footer-column">
           <ul>
-            <li><a href="#ofislar">Bank ofislari va bankomatlar</a></li>
-            <li><a href="#kurslar">Valyutalar kursi</a></li>
-            <li><a href="#kabinet">Shaxsiy kabinet</a></li>
-            <li><a href="#qayta-aloqa">Qayta aloqa</a></li>
+            <li>
+              <a href="#ofislar">Bank ofislari va bankomatlar</a>
+            </li>
+            <li>
+              <a href="#kurslar">Valyutalar kursi</a>
+            </li>
+            <li>
+              <a href="#kabinet">Shaxsiy kabinet</a>
+            </li>
+            <li>
+              <a href="#qayta-aloqa">Qayta aloqa</a>
+            </li>
           </ul>
         </div>
 
-
         <div className="footer-column">
           <ul>
-            <li><a href="#jismoniy">Jismoniy shaxslar uchun</a></li>
-            <li><a href="#biznes">Biznes uchun</a></li>
-            <li><a href="#moliyaviy">Moliyaviy tashkilotlarga</a></li>
-            <li><a href="#haqida">Bank haqida</a></li>
+            <li>
+              <a href="#jismoniy">Jismoniy shaxslar uchun</a>
+            </li>
+            <li>
+              <a href="#biznes">Biznes uchun</a>
+            </li>
+            <li>
+              <a href="#moliyaviy">Moliyaviy tashkilotlarga</a>
+            </li>
+            <li>
+              <a href="#haqida">Bank haqida</a>
+            </li>
           </ul>
         </div>
 
         <div className="footer-column">
           <ul>
-            <li><a href="#matbuot">Matbuot markazi</a></li>
-            <li><a href="#rekvizitlar">Rekvizitlar</a></li>
-            <li><a href="#internet-bank">Internet-bank</a></li>
-            <li><a href="#sayt-xaritasi">Sayt xaritasi</a></li>
+            <li>
+              <a href="#matbuot">Matbuot markazi</a>
+            </li>
+            <li>
+              <a href="#rekvizitlar">Rekvizitlar</a>
+            </li>
+            <li>
+              <a href="#internet-bank">Internet-bank</a>
+            </li>
+            <li>
+              <a href="#sayt-xaritasi">Sayt xaritasi</a>
+            </li>
           </ul>
         </div>
 
         <div className="footer-column mobile-bank-column">
           <div className="mobile-bank-container">
             <div className="bank-logo-wrapper">
-              <div className="bank-logo"><img src={logo} alt="" /></div> 
+              <div className="bank-logo">
+                <img src={logo} alt="" />
+              </div>
             </div>
             <div className="mobile-bank-text">
               <h4>Mobil bank</h4>
@@ -55,13 +78,14 @@ const Footer = () => {
             </div>
           </div>
         </div>
-
       </div>
 
       <hr className="footer-divider" />
 
       <div className="footer-bottom">
-        <button className="corruption-btn">Korrupsiya haqida xabar berish</button>
+        <button className="corruption-btn">
+          Korrupsiya haqida xabar berish
+        </button>
 
         <div className="social-icons">
           <a href="#facebook" className="social-link">
