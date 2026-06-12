7270656c9137b755f582b9768b7f2a1a7dc0e916
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-06-12 11:22:59 +0500

    salom

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,15 +1,10 @@
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaYoutube,
-  FaTelegramPlane,
-} from "react-icons/fa";
 
+import logo from "./assets/mobile_app.svg";
 const Footer = () => {
   return (
     <footer className="bank-footer">
       <div className="footer-container">
-        {/* 1-Ustun: Aloqa va Ishonch telefonlari */}
+        
         <div className="footer-column contact-column">
           <div className="phone-group">
             <span className="phone-number">0 (800) 1 200 200, 1256</span>
@@ -21,66 +16,38 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* 2-Ustun: Bank xizmatlari */}
         <div className="footer-column">
           <ul>
-            <li>
-              <a href="#ofislar">Bank ofislari va bankomatlar</a>
-            </li>
-            <li>
-              <a href="#kurslar">Valyutalar kursi</a>
-            </li>
-            <li>
-              <a href="#kabinet">Shaxsiy kabinet</a>
-            </li>
-            <li>
-              <a href="#qayta-aloqa">Qayta aloqa</a>
-            </li>
+            <li><a href="#ofislar">Bank ofislari va bankomatlar</a></li>
+            <li><a href="#kurslar">Valyutalar kursi</a></li>
+            <li><a href="#kabinet">Shaxsiy kabinet</a></li>
+            <li><a href="#qayta-aloqa">Qayta aloqa</a></li>
           </ul>
         </div>
 
-        {/* 3-Ustun: Mijozlar turi */}
+
         <div className="footer-column">
           <ul>
-            <li>
-              <a href="#jismoniy">Jismoniy shaxslar uchun</a>
-            </li>
-            <li>
-              <a href="#biznes">Biznes uchun</a>
-            </li>
-            <li>
-              <a href="#moliyaviy">Moliyaviy tashkilotlarga</a>
-            </li>
-            <li>
-              <a href="#haqida">Bank haqida</a>
-            </li>
+            <li><a href="#jismoniy">Jismoniy shaxslar uchun</a></li>
+            <li><a href="#biznes">Biznes uchun</a></li>
+            <li><a href="#moliyaviy">Moliyaviy tashkilotlarga</a></li>
+            <li><a href="#haqida">Bank haqida</a></li>
           </ul>
         </div>
 
-        {/* 4-Ustun: Ma'lumotlar */}
         <div className="footer-column">
           <ul>
-            <li>
-              <a href="#matbuot">Matbuot markazi</a>
-            </li>
-            <li>
-              <a href="#rekvizitlar">Rekvizitlar</a>
-            </li>
-            <li>
-              <a href="#internet-bank">Internet-bank</a>
-            </li>
-            <li>
-              <a href="#sayt-xaritasi">Sayt xaritasi</a>
-            </li>
+            <li><a href="#matbuot">Matbuot markazi</a></li>
+            <li><a href="#rekvizitlar">Rekvizitlar</a></li>
+            <li><a href="#internet-bank">Internet-bank</a></li>
+            <li><a href="#sayt-xaritasi">Sayt xaritasi</a></li>
           </ul>
         </div>
 
-        {/* 5-Ustun: Mobil Bank ilovasi */}
         <div className="footer-column mobile-bank-column">
           <div className="mobile-bank-container">
             <div className="bank-logo-wrapper">
-              {/* Bu yerga yashil rangli "H" logotipini qo'yasiz */}
-              <div className="bank-logo">H</div>
+              <div className="bank-logo"><img src={logo} alt="" /></div> 
             </div>
             <div className="mobile-bank-text">
               <h4>Mobil bank</h4>
@@ -88,28 +55,26 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
       </div>
 
       <hr className="footer-divider" />
 
-      {/* Pastki qism: Korrupsiya tugmasi va Ijtimoiy tarmoqlar */}
       <div className="footer-bottom">
-        <button className="corruption-btn">
-          Korrupsiya haqida xabar berish
-        </button>
+        <button className="corruption-btn">Korrupsiya haqida xabar berish</button>
 
         <div className="social-icons">
           <a href="#facebook" className="social-link">
-            <FaFacebookF />
+            <i className="ri-facebook-fill"></i>
           </a>
           <a href="#instagram" className="social-link">
-            <FaInstagram />
+            <i className="ri-instagram-line"></i>
           </a>
           <a href="#youtube" className="social-link">
-            <FaYoutube />
+            <i className="ri-youtube-fill"></i>
           </a>
           <a href="#telegram" className="social-link">
-            <FaTelegramPlane />
+            <i className="ri-telegram-2-fill"></i>
           </a>
         </div>
       </div>
