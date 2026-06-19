8bd91c02a2e1e5f290cf36b0bd6e4ef0082508fc
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-06-19 11:04:12 +0500

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
 import Grid from "./Grid";
@@ -5,8 +6,51 @@ import Visa from "./Visa";
 import Footer from "./Footer";
 
 const App = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const online = () => {
+      setIsOnline(true);
+      setShowMessage(true);
+
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
+    };
+
+    const offline = () => {
+      setIsOnline(false);
+    };
+
+    window.addEventListener("online", online);
+    window.addEventListener("offline", offline);
+
+    return () => {
+      window.removeEventListener("online", online);
+      window.removeEventListener("offline", offline);
+    };
+  }, []);
+
+  if (!isOnline) {
+    return (
+      <div className="offline">
+        <div className="spinner"></div>
+        <h1>Siz oflaynsiz</h1>
+        <p>Internetni tekshiring...</p>
+      </div>
+    );
+  }
+
   return (
     <>
+      
+      {showMessage && (
+        <div className="toast">
+          ✅ Siz yana onlaynsiz
+        </div>
+      )}
+
       <Navbar />
       <Cart />
       <Grid />
